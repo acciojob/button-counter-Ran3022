7dbd97131f3d3c6e0e58cb5dd99520cc2fff8e2e
ran3022 <ranjeetkumar0591132@gmail.com>
2024-01-23 19:54:39 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
-
-import React from "react";
 import './../styles/App.css';
 import React, { useState } from "react";
 
-const App = () => {
-  return (
-    <div>
-      function MyButton() {
-  const [count, setCount] = useState(0);
+const App=()=>{
+let [count,setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1)
-  }
 
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
+function addCount(){
+  setCount(count+1);
 }
-    </div>
-  )
+
+
+  return(<div>
+    <p>
+      Button Clicked {count} times
+    </p>
+    <button onClick={addCount}>Click me</button>
+  </div>)
 }
 
 export default App
